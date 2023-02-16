6ef26dd4f8babd8d8d35bb3c6ac876bcca7feb44
Kirill Makhnev <kalorigen@gmail.com>
2023-02-16 14:16:48 +0300

    configured tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import HomePage from "@pages/Home/Home";
-import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import HomePage from '@pages/Home/Home'
+import { Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="wrapper">
-      <div className="content">
-        <Suspense fallback={<div className=""></div>}>
+    <div className='wrapper'>
+      <div className='content'>
+        <Suspense fallback={<div className=''></div>}>
           <Routes>
-            <Route path="*" element={<HomePage />} />
+            <Route path='*' element={<HomePage />} />
           </Routes>
         </Suspense>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
